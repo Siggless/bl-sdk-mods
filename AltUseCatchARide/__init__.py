8a0119f1a41dc9e5a37a4d230b0c6bca9e8afb0a
Siggless <157992406+Siggless@users.noreply.github.com>
2024-11-25 19:19:52 +0000

    AltUseCatchARide v1.1.0
    Added options for which vehicle type to spawn.

--- AltUseCatchARide/__init__.py
+++ AltUseCatchARide/__init__.py
@@ -21,44 +21,45 @@ except ImportError:
     raise ImportError("Alt Use Catch-a-Ride requires at least Enums version 1.0")
 
 
-FAMILY_DICT: Dict[Game, Dict[str,(VehicleFamilyDefinition, str, VSSUIDefinition)]] = {
+FAMILY_DICT: Dict[str, (VehicleFamilyDefinition, str, Dict[str, VSSUIDefinition])] = {
     Game.BL2: {
-        "Technical":("GD_Globals.VehicleSpawnStation.VehicleFamily_BanditTechnical","VehicleFamily_BanditTechnical",[
-            "GD_Globals.VehicleSpawnStation.VSSUI_SawBladeTechnical",
-            "GD_Globals.VehicleSpawnStation.VSSUI_CatapultTechnical"
-            ]),
-        "Runner":("GD_Globals.VehicleSpawnStation.VehicleFamily_Runner","VehicleFamily_Runner",[
-            "GD_Globals.VehicleSpawnStation.VSSUI_MGRunner",
-            "GD_Globals.VehicleSpawnStation.VSSUI_RocketRunner"
-            ]),
-        "Sandskiff":("GD_OrchidPackageDef.Vehicles.VehicleFamily_Hovercraft","VehicleFamily_Hovercraft",[
-            "GD_OrchidPackageDef.Vehicles.VSSUI_HarpoonHovercraft","GD_OrchidPackageDef.Vehicles.VSSUI_RocketHovercraft",
-            "GD_OrchidPackageDef.Vehicles.VSSUI_SawBladeHovercraft"
-            ]),
-        "Fanboat":("GD_SagePackageDef.Vehicles.VehicleFamily_FanBoat","VehicleFamily_FanBoat",[
-            "GD_SagePackageDef.Vehicles.VSSUI_CorrosiveFanBoat",
-            "GD_SagePackageDef.Vehicles.VSSUI_IncendiaryFanBoat",
-            "GD_SagePackageDef.Vehicles.VSSUI_ShockFanBoat"
-            ]),
+        "Technical": ("GD_Globals.VehicleSpawnStation.VehicleFamily_BanditTechnical","VehicleFamily_BanditTechnical",{
+            "Saw Blade": "GD_Globals.VehicleSpawnStation.VSSUI_SawBladeTechnical",
+            "Catapult": "GD_Globals.VehicleSpawnStation.VSSUI_CatapultTechnical"
+        }),
+        "Runner": ("GD_Globals.VehicleSpawnStation.VehicleFamily_Runner","VehicleFamily_Runner",{
+            "Machine Gun": "GD_Globals.VehicleSpawnStation.VSSUI_MGRunner",
+            "Rocket Launcher": "GD_Globals.VehicleSpawnStation.VSSUI_RocketRunner"
+        }),
+        "Sandskiff": ("GD_OrchidPackageDef.Vehicles.VehicleFamily_Hovercraft","VehicleFamily_Hovercraft",{
+            "Harpoon": "GD_OrchidPackageDef.Vehicles.VSSUI_HarpoonHovercraft",
+            "Rocket Launcher": "GD_OrchidPackageDef.Vehicles.VSSUI_RocketHovercraft",
+            "Saw Blade": "GD_OrchidPackageDef.Vehicles.VSSUI_SawBladeHovercraft"
+        }),
+        "Fanboat": ("GD_SagePackageDef.Vehicles.VehicleFamily_FanBoat","VehicleFamily_FanBoat",{
+            "Corrosive": "GD_SagePackageDef.Vehicles.VSSUI_CorrosiveFanBoat",
+            "Incendiary": "GD_SagePackageDef.Vehicles.VSSUI_IncendiaryFanBoat",
+            "Shock": "GD_SagePackageDef.Vehicles.VSSUI_ShockFanBoat"
+        }),
     },
     Game.TPS: {
-        "MoonBuggy":("GD_Globals.VehicleSpawnStation.VehicleFamily_MoonBuggy","VehicleFamily_MoonBuggy",[
-            "GD_Globals.VehicleSpawnStation.VSSUI_LaserBuggy",
-            "GD_Globals.VehicleSpawnStation.VSSUI_MissileBuggy"
-            ]),
-        "StingRay":("GD_Globals.VehicleSpawnStation.VehicleFamily_StingRay","VehicleFamily_StingRay",[
-            "GD_Globals.VehicleSpawnStation.VSSUI_StingRay_CryoRocket",
-            "GD_Globals.VehicleSpawnStation.VSSUI_StingRay_FlakBurst"
-            ]),
+        "Moon Buggy": ("GD_Globals.VehicleSpawnStation.VehicleFamily_MoonBuggy","VehicleFamily_MoonBuggy",{
+            "Laser": "GD_Globals.VehicleSpawnStation.VSSUI_LaserBuggy",
+            "Missile": "GD_Globals.VehicleSpawnStation.VSSUI_MissileBuggy"
+        }),
+        "Stingray": ("GD_Globals.VehicleSpawnStation.VehicleFamily_StingRay","VehicleFamily_StingRay",{
+            "Cryo": "GD_Globals.VehicleSpawnStation.VSSUI_StingRay_CryoRocket",
+            "Flak": "GD_Globals.VehicleSpawnStation.VSSUI_StingRay_FlakBurst"
+        }),
     }
 }[Game.GetCurrent()]
 DEFAULTS_DICT = {
-    "Game.BL2" : "Runner",
-    "Game.TPS" : "MoonBuggy",
+    "Game.BL2": "Runner",
+    "Game.TPS": "Moon Buggy",
     # DLC codes override the game defaults
-    "Orchid" : "Sandskiff",
-    "Sage" : "Fanboat",
-    }
+    "Orchid": "Sandskiff",
+    "Sage": "Fanboat",
+}
 
 
 class AltUseCatchARide(SDKMod):
@@ -66,51 +67,63 @@ class AltUseCatchARide(SDKMod):
     Author: str = "Siggles" # But based on Alt Use Vendors mod, check that out
     Description: str = (
         "Adds alt use binds to Catch-a-Ride stations, to instantly deploy a vehicle.\n\n"
-        "Options to teleport to vehicle on spawn, and choose a vehicle family (including DLC vehicles)."
+        "Options to teleport to vehicle on spawn, choose a vehicle family (including DLC vehicles), and choose the vehicle weapon to use for each family."
     )
-    Version: str = "1.0.1"
+    Version: str = "1.1.0"
 
     Types: ModTypes = ModTypes.Utility
     SaveEnabledState: EnabledSaveType = EnabledSaveType.LoadWithSettings
     SupportedGames: Game = Game.BL2 | Game.TPS  # NOT AoDK
 
-    customIcon:InteractionIconDefinition
-    MAX_SLOTS:int=2
-
-    vehicleType:str = "Default"
-    autoTeleport:bool = False
-    
+    customIcon: InteractionIconDefinition
+    MAX_SLOTS: int = 2  # Stringray has 4 but whatevs
 
     def __init__(self) -> None:
         super().__init__()
-        spinnerChoices = list(FAMILY_DICT.keys())
-        spinnerChoices.append("Default")
-        self.SpinnerVehicleType = Options.Spinner(
-            Caption="Vehicle Type",
-            Description="The type of vehicle to spawn. Default is the last chosen vehicle.",
-            StartingValue="Default",
-            Choices=spinnerChoices
-        )
+        
         self.BoolAutoTeleport = Options.Boolean(
             Caption="Auto Teleport",
             Description="If YES, teleports you to the driver seat on spawn.",
             StartingValue=False,
             Choices=["No", "Yes"]  # False, True
         )
-        self.Options = [
-            self.SpinnerVehicleType,
-            self.BoolAutoTeleport
-        ]
         
-    def ModOptionChanged(self, option: Options.Base, new_value) -> None:
-        if option == self.SpinnerVehicleType:
-            self.vehicleType = new_value
-        if option == self.BoolAutoTeleport:
-            self.autoTeleport = new_value
+        familyChoices = list(FAMILY_DICT.keys())
+        familyChoices.append("Default")
+        self.SpinnerVehicleFamily = Options.Spinner(
+            Caption="Vehicle Family",
+            Description="The family of vehicle to spawn. Default is the last chosen vehicle.",
+            StartingValue="Default",
+            Choices=familyChoices
+        )
+        
+        self.familyLoadoutDict: Dict[str, Options.Nested] = {}
+        for family in FAMILY_DICT:
+            familyTypes = list(FAMILY_DICT[family][2].keys())
+            spinner = Options.Spinner(
+                Caption=f"{family} Type",
+                Description=f"The type of {family} to spawn.",
+                StartingValue=familyTypes[0],
+                Choices=familyTypes
+            )
+            self.familyLoadoutDict[family] = spinner
+
+        self.NestedVehicleTypes = Options.Nested(
+            Caption="Vehicle Types",
+            Description="Options menu for which type of vehicle is spawned, for each vehicle family.",
+            Children=list(self.familyLoadoutDict.values()),
+            IsHidden=False
+        )
+        
+        self.Options = [
+            self.BoolAutoTeleport,
+            self.SpinnerVehicleFamily,
+            self.NestedVehicleTypes
+        ]
 
     def Enable(self) -> None:
         super().Enable()
-        unrealsdk.GetEngine().GamePlayers[0].Actor.StopUsingVehicleSpawnStationTerminal()
+        #unrealsdk.GetEngine().GamePlayers[0].Actor.StopUsingVehicleSpawnStationTerminal()
         self.CreateIcon()
 
     def CreateIcon(self) -> None:
@@ -121,7 +134,7 @@ class AltUseCatchARide(SDKMod):
         """
 
         baseIcon = unrealsdk.FindObject("InteractionIconDefinition", "GD_InteractionIcons.Default.Icon_DefaultUse")
-        customIconName="Mod_Icon_Driver"
+        customIconName = "Mod_Icon_Driver"
         # Check if the icon already exists (mod re-enabled) and don't recreate if so
         icon = unrealsdk.FindObject("InteractionIconDefinition", f"GD_InteractionIcons.Default.{customIconName}")
         if icon is None:
@@ -141,7 +154,7 @@ class AltUseCatchARide(SDKMod):
             PC.ServerRCon(f"set {PC.PathName(icon)} Action UseSecondary")
             PC.ServerRCon(f"set {PC.PathName(icon)} Text QUICK DEPLOY")
 
-        self.customIcon=icon
+        self.customIcon = icon
 
     @Hook("WillowGame.WillowInteractiveObject.InitializeFromDefinition")
     def InitializeFromDefinition(self, caller: unrealsdk.UObject, function: unrealsdk.UFunction, params: unrealsdk.FStruct) -> bool:
@@ -188,14 +201,11 @@ class AltUseCatchARide(SDKMod):
         familyDef = self.GetVehicleFamily(caller)
         caller.RouteCallToSetVehicleFamily(familyDef)   # Update the family stored in the game, as we aren't going through the UI.
         
-        # Get the vehicle loadout - I'm just defaulting to the first option for now!
-        vuiDef:VSSUIDefinition
-        for i in FAMILY_DICT:
-            if FAMILY_DICT[i][1] == familyDef.Name:
-                vuiDef=unrealsdk.FindObject("VSSUIDefinition", FAMILY_DICT[i][2][0])
-                break
+        # Get the vehicle loadout
+        vuiDef:VSSUIDefinition = self.GetVehicleLoadout(familyDef)
         if vuiDef is None:
-            unrealsdk.Log("["+self.Name+"] No loadout for vehicle family "+ str(familyDef.Name))
+            unrealsdk.Log("["+self.Name+"] No loadout for vehicle family " + str(familyDef.Name))
+            caller.StopUsingVehicleSpawnStationTerminal()
             return False
 
         # Get the customisation for the given slot
@@ -206,12 +216,12 @@ class AltUseCatchARide(SDKMod):
         caller.SpawnVehicleFromConnectedVehicleSpawnStationTerminal(slot, vuiDef, customizationDef)
 
         # Optional teleport to this vehicle
-        if self.autoTeleport:
+        if self.BoolAutoTeleport.CurrentValue:
             #caller.ServerTryToTeleportToVehicle(slot)
             # Dunno why they use a delay but best be safe an do the same
-            caller.VSSSlotIndexForDelayedTeleport = slot;
-            caller.TheVSSUIMovie = None;
-            caller.SetTimer(0.25, False, 'DelayedTeleportToVehicle'); 
+            caller.VSSSlotIndexForDelayedTeleport = slot
+            caller.TheVSSUIMovie = None
+            #caller.SetTimer(0.25, False, 'DelayedTeleportToVehicle')   # Doesn't work
             caller.DelayedTeleportToVehicle()
             
         caller.StopUsingVehicleSpawnStationTerminal()
@@ -226,13 +236,13 @@ class AltUseCatchARide(SDKMod):
         """
         popMaster = PC.GetWillowGlobals().GetPopulationMaster()
         # If a slot is not deployed then use that
-        for s in range (self.MAX_SLOTS):
+        for s in range(self.MAX_SLOTS):
             vehicleFromSlot = popMaster.GetVehicleFromVehicleSpawnStation(s)
             if vehicleFromSlot is None:
                 return s
             
         # If all vehicles are deployed then we look for the first unoccupied vehicle to redeploy
-        for s in range (self.MAX_SLOTS):
+        for s in range(self.MAX_SLOTS):
             vehicleFromSlot = popMaster.GetVehicleFromVehicleSpawnStation(s)
             if not vehicleFromSlot.Occupied():
                 return s
@@ -242,16 +252,16 @@ class AltUseCatchARide(SDKMod):
     
     def GetVehicleFamily(self, PC: WillowPlayerController) -> VehicleFamilyDefinition:
         """
-        Returns the VehicleFamilyDefition class set in the mod options.
+        Returns the VehicleFamilyDefition set in the mod options.
         Or if "Default" is set, then the current family given by the game.
-        Or if not family has been chosen in-game yet, then a default family for the current DLC.
+        Or if no family has been chosen in-game yet, then a default family for the current DLC.
         """
         familyDef:VehicleFamilyDefinition
-        if self.vehicleType == "Default" or self.vehicleType not in FAMILY_DICT:
+        if self.SpinnerVehicleFamily.CurrentValue == "Default" or self.SpinnerVehicleFamily.CurrentValue not in FAMILY_DICT:
             # Default to the family given by the game
             familyDef = PC.GetWillowGlobals().GetVehicleLifetimeManager().CurrentVehicleFamily
         else:
-            familyDef = unrealsdk.FindObject("VehicleFamilyDefinition", FAMILY_DICT[self.vehicleType][0])
+            familyDef = unrealsdk.FindObject("VehicleFamilyDefinition", FAMILY_DICT[self.SpinnerVehicleFamily.CurrentValue][0])
             
         if familyDef is None:
             # No vehicle family has been chosen yet in-game, so we need to set a default
@@ -265,6 +275,22 @@ class AltUseCatchARide(SDKMod):
             familyDef = unrealsdk.FindObject("VehicleFamilyDefinition", FAMILY_DICT[chosenDefaultFamily][0])
             
         return familyDef
+    
+    def GetVehicleLoadout(self, familyDef: VehicleFamilyDefinition) -> VSSUIDefinition:
+        """
+        Returns the VSSUIDefinition set in the mod options for the given family.
+        This must be defined in the mod options, since I can't easily get the GfxMovie from the InteractiveObject,
+         to get the VSSUIDefinition from the ChoiceModule in that, if the family matches.
+        """
+        for family in FAMILY_DICT:
+            if FAMILY_DICT[family][1] == familyDef.Name:
+                familyTypes = FAMILY_DICT[family][2]
+                familySpinner = self.familyLoadoutDict[family]
+                if familySpinner.CurrentValue in familyTypes:
+                    return unrealsdk.FindObject("VSSUIDefinition", familyTypes[familySpinner.CurrentValue])
+            
+        unrealsdk.Log("["+self.Name+"] No mod options spinner found for vehicle family " + str(familyDef.Name))
+        return None
 
 # END CLASS AltUseCatchARide
 
