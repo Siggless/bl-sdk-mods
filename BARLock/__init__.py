9d0840f3bddce4158fd29cea4e44a096865c925f
Siggless <157992406+Siggless@users.noreply.github.com>
2025-06-07 23:36:37 +0100

    Add files via upload

--- BARLock/__init__.py
+++ BARLock/__init__.py
@@ -1,105 +1,56 @@
-import unrealsdk
-from typing import Any, Dict
-from Mods import ModMenu, OptionManager
-from Mods.ModMenu import EnabledSaveType, Game, ModTypes, Hook, SDKMod
+from unrealsdk.hooks import Type, Block
+from unrealsdk.unreal import BoundFunction, UObject, WrappedStruct
+from mods_base import build_mod, hook
+from mods_base.options import BoolOption
+from typing import Any
+
+optionLockBonusStats = BoolOption(
+    "Lock Bonus Stats", False,
+    description="Whether BAR bonus stats can be enabled in the in-game menu."
+)
 
 
-class BARLock(SDKMod):
-    Name: str = "Badass Rank Lock"
-    Author: str = "Siggles"
-    Description: str = "Prevents badass rank increasing, and non-level-specific challenge reward pop-ups from showing.\n\nSetting to prevent enabling badass bonus stats (in this menu)."
-    Version: str = "1.0.0"
-    SupportedGames: Game = Game.BL2 | Game.TPS | Game.AoDK
-    Types: ModTypes = ModTypes.Utility
-    SaveEnabledState: EnabledSaveType = EnabledSaveType.LoadOnMainMenu
-    
-    SettingsInputs: Dict[str, str] = {
-        "Enter": "Enable",
-        "B": "Bonus Stats: Locked"
-    }
-    
-    def __init__(self) -> None:
-        self.LockBonusStats = ModMenu.Options.Hidden(
-            Caption="Lock Bonus Stats",
-            Description="Whether BAR bonus stats can be enabled in the in-game menu.",
-            StartingValue=True,
-        )
-        self.Options = [
-            self.LockBonusStats,
-        ]
-        
-    def ModOptionChanged(self, option: OptionManager.Options.Base, new_value: Any) -> None:
-        if option is self.LockBonusStats:
-            if new_value:
-                self.SettingsInputs["B"] = "Bonus Stats: Locked"
-            else:
-                self.SettingsInputs["B"] = "Bonus Stats: Available"
-        else:
-            super.ModOptionChanged()
+@hook("WillowGame.WillowPlayerController:AdjustBadassPoints")
+def AdjustBadassPoints(obj: UObject, __args: WrappedStruct, __ret: Any, __func: BoundFunction) -> Block | None:
+    ''' Don't add ranks for challenge completion '''
+    return Block
+
+
+@hook("WillowGame.WillowPlayerController:ApplyAwesomeSkillSaveGameData")
+def ApplyAwesomeSkillSaveGameData(obj: UObject, __args: WrappedStruct, __ret: Any, __func: BoundFunction) -> Block | None:
+    ''' Don't enable badass rank on load and unregister non-level-specific challenges '''
+    if optionLockBonusStats.value:
+        obj.bAwesomeSkillDisabled = True
+    # This is my last resort to prevent the pop-ups - Stops the progress update notifications but also stops customisation rewards.
+    obj.ClearActivePlayerChallenges()
+    return Block
+
+
+@hook("WillowGame.BadassPanelGFxObject:ToggleBadassSkill")
+def ToggleBadassSkill(obj: UObject, __args: WrappedStruct, __ret: Any, __func: BoundFunction) -> Block | None:
+    ''' Prevent enabling BAR '''
+    if optionLockBonusStats.value and obj.OwningMovie.WPCOwner.IsBadassSkillDisabled():
+        obj.ParentPanel.ParentMovie.PlayUISound('ResultFailure')
+        return Block
+    return True
+
+
+@hook("WillowGame.BadassPanelGFxObject:SetInitialButtonStates", Type.POST)
+def SetInitialButtonStates(obj: UObject, __args: WrappedStruct, __ret: Any, __func: BoundFunction):
+    ''' Change the title of the badass menu '''
+    obj.GetObject("badassRankTitle").SetString("text", obj.BA_RankString + " [LOCKED]")
+
+
+@hook("WillowGame.BadassPanelGFxObject:UpdateRedeemTokensFocusedTooltips", Type.POST)
+def UpdateRedeemTokensFocusedTooltips(obj: UObject, __args: WrappedStruct, __ret: Any, __func: BoundFunction):
+    ''' Change the tooltip in the badass menu '''
+    if not optionLockBonusStats.value:
         return
-    
-    def SettingsInputPressed(self, action: str) -> None:
-        if action == "Bonus Stats: Locked":
-            self.ModOptionChanged(self.LockBonusStats, False)
-            self.LockBonusStats.CurrentValue = False
-        elif action == "Bonus Stats: Available":
-            self.ModOptionChanged(self.LockBonusStats, True)
-            self.LockBonusStats.CurrentValue = True
-        else:
-            super().SettingsInputPressed(action)
 
+    currentTip = str(obj.OwningMovie.GetVariableString("tooltips.tooltips.htmlText"))
+    splitty = str(obj.TooltipsText_ActivateBonusStats).split("> ")
+    tipCaption = splitty[-1]
+    obj.OwningMovie.SetVariableString("tooltips.tooltips.htmlText", currentTip.replace(tipCaption,"<font color='#666666'>[LOCKED]</font>"));
+   
 
-    @Hook("WillowGame.WillowPlayerController.AdjustBadassPoints")
-    def AdjustBadassPoints(self, caller: unrealsdk.UObject, function: unrealsdk.UFunction, params: unrealsdk.FStruct) -> bool:
-        ''' Don't add ranks for challenge completion '''
-        return False
-    
-    @Hook("WillowGame.WillowPlayerController.ApplyAwesomeSkillSaveGameData")
-    def ApplyAwesomeSkillSaveGameData(self, caller: unrealsdk.UObject, function: unrealsdk.UFunction, params: unrealsdk.FStruct) -> bool:
-        ''' Don't enable badass rank on load and unregister non-level-specific challenges '''
-        if self.LockBonusStats.CurrentValue == True:
-            caller.bAwesomeSkillDisabled = True
-        # This is my last resort to prevent the pop-ups - Stops the progress update notifications but also stops customisation rewards.
-        caller.ClearActivePlayerChallenges()
-        return False
-    
-    @Hook("WillowGame.BadassPanelGFxObject.ToggleBadassSkill")
-    def ToggleBadassSkill(self, caller: unrealsdk.UObject, function: unrealsdk.UFunction, params: unrealsdk.FStruct) -> bool:
-        ''' Prevent enabling BAR '''
-        if self.LockBonusStats.CurrentValue == False:
-            return True
-        if caller.OwningMovie.WPCOwner.IsBadassSkillDisabled():
-            caller.ParentPanel.ParentMovie.PlayUISound('ResultFailure')
-            return False
-        return True
-    
-    @Hook("WillowGame.BadassPanelGFxObject.SetInitialButtonStates")
-    def SetInitialButtonStates(self, caller: unrealsdk.UObject, function: unrealsdk.UFunction, params: unrealsdk.FStruct) -> bool:
-        ''' Change the title of the badass menu '''
-        caller.GetObject("badassRankTitle").SetString("text", caller.BA_RankString + " [LOCKED]");
-        return True
-    
-    @Hook("WillowGame.BadassPanelGFxObject.UpdateRedeemTokensFocusedTooltips")
-    def UpdateRedeemTokensFocusedTooltips(self, caller: unrealsdk.UObject, function: unrealsdk.UFunction, params: unrealsdk.FStruct) -> bool:
-        ''' Change the tooltip in the badass menu '''
-        if self.LockBonusStats.CurrentValue == False:
-            return True
-
-        unrealsdk.DoInjectedCallNext()
-        caller.UpdateRedeemTokensFocusedTooltips(caller, function, params)
-
-        currentTip = str(caller.OwningMovie.GetVariableString("tooltips.tooltips.htmlText"))
-        splitty = str(caller.TooltipsText_ActivateBonusStats).split("> ")
-        tipCaption = splitty[-1]
-        if len(splitty) <= 2:
-            # This is the original string (no UCP enabled shenanigans)
-            caller.OwningMovie.SetVariableString("tooltips.tooltips.htmlText", currentTip.replace(tipCaption,"<font color='#666666'>[LOCKED]</font>"));
-        # else this is not the original string (changed by UCP to add the enabled message), and setting this again causes problems, so naaah
-
-        return False
-    
-### End class BARLock
-
-
-instance = BARLock()
-ModMenu.RegisterMod(instance)
+build_mod()
