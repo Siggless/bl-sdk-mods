2391b72ca0f25b37688ed8fd759d120d404a9eaf
Siggless <157992406+Siggless@users.noreply.github.com>
2025-06-07 23:37:22 +0100

    Add files via upload

--- GrenadeModQuickSwitcher/__init__.py
+++ GrenadeModQuickSwitcher/__init__.py
@@ -1,37 +1,19 @@
-import unrealsdk
-from Mods.ModMenu import EnabledSaveType, Game, ModTypes, SDKMod, RegisterMod, Keybind
+from mods_base import EInputEvent, build_mod, get_pc, keybind
 
-
-class GrenadeModQuickSwitcher(SDKMod):
-    Name: str = "Grenade Mod Quick Switcher"
-    Author: str = "Siggles"
-    Description: str = "Adds a keybind that cycles through grenade mods marked as favourite.\n" \
-        "Cycles in the order that items were added to the backpack, or unequipped."
-    Version: str = "1.0.0"
-    SupportedGames: Game = Game.BL2 | Game.TPS | Game.AoDK
-    Types: ModTypes = ModTypes.Utility
-    SaveEnabledState: EnabledSaveType = EnabledSaveType.LoadOnMainMenu
+@keybind("Switch Grenade Mod")
+def SwitchGrenadeMod():
+    PC = get_pc()
+    InvManager = PC.GetPawnInventoryManager()
     
+    # Since the readied item gets added to the end of the backpack list,
+    # the backpack acts as a queue, so we don't have to mess with cycling order. Yay!
+    for inv in InvManager.Backpack:
+        if inv is not None:
+            if not inv.bReadied and inv.GetCategoryKey() == "mod" and inv.Mark == 2:
+                InvManager.ReadyBackpackInventory(inv)
+                PC.PlaySpecialHUDSound('RewardToken')
+                break
 
-    def SwitchGrenade():
-        """ Called from modded keybind """
-        PC = unrealsdk.GetEngine().GamePlayers[0].Actor
-        InvManager = PC.GetPawnInventoryManager()
-        
-        # Since the readied item gets added to the end of the backpack list, the backpack acts as a queue, so we don't have to mess with cycling order. Yay!
-        for inv in InvManager.Backpack:
-            if inv is not None:
-                if not inv.bReadied and inv.GetCategoryKey()=="mod" and inv.Mark == 2:
-                    InvManager.ReadyBackpackInventory(inv)
-                    PC.PlaySpecialHUDSound('RewardToken')
-                    break
+    return
 
-        return
-    
-    Keybinds = [Keybind("Switch Grenade Mod", "V", OnPress=SwitchGrenade)]    
-
-### End class GrenadeModQuickSwitcher
-
-
-instance = GrenadeModQuickSwitcher()
-RegisterMod(instance)
+build_mod()
